6e8e54841bfaa02e02a7b757342f04e30c35c7db
Vishwanath Hegde <vishwahegde27@gmail.com>
2024-09-06 23:41:54 +0530

    NGRX Router-Store

--- src/app/store/router/router.selector.ts
+++ src/app/store/router/router.selector.ts
@@ -2,7 +2,6 @@ import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { Router } from "./custom-serializer";
 import { RouterReducerState } from "@ngrx/router-store";
 
-
 const getRouterState = createFeatureSelector<RouterReducerState<Router>>('router');
 
 export const getRouterInfo = createSelector(getRouterState, (state) => {
